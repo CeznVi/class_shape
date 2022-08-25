0bf359e1046ac297dd59afc792ffab5d82aca855
Viktor <Cezn.Vi@gmail.com>
2022-08-25 21:05:09 +0300

    v0.1

--- shape.h
+++ shape.h
@@ -1,1 +1,16 @@
-#pragma once
+﻿#pragma once
+#include <string>
+
+//абстрактний клас
+class Shape
+{
+protected:
+	string name = "no_name";
+	int x{}, y{};
+public:
+	virtual void Show() = 0;
+	virtual void Save() = 0;
+	virtual void Load() = 0;
+};
+
+
