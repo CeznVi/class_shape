bc6d5a7e8c6ab713cf6f3d7a16bd1015b53cb29e
Viktor <Cezn.Vi@gmail.com>
2022-08-26 21:05:53 +0300

    final

--- Class_shape.cpp
+++ Class_shape.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include<Windows.h>
 #include "shape.h"
+#include "func.h"
 
 using namespace std;
 
@@ -9,14 +10,41 @@ int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-
-    cout << "Тест\n";
-    Shape* s;
-    Square sq{10, 5, 12};
     
-    s = &sq;
-    s->Show();
-    s->Save();
+    //розмір масиву
+    const int SIZE = 4;
 
-    return 0;
+    //Cтворюємо фігури
+    Square sq{ 22, 4, 30 };
+    Reactangle react{ 5,8,15,5 };
+    Circle circl{ 122, 34, 20 };
+    Elipse elips{ 0, 0, 13, 8};
+     
+    Shape* arrShape[SIZE]{};
+       
+    arrShape[0] = &sq;
+    arrShape[1] = &react;
+    arrShape[2] = &circl;
+    arrShape[3] = &elips;
+
+   //роздрукувати
+   print(arrShape, SIZE);
+   //очистити файл
+   clearFile();
+   //зберегти все в файл
+   saveToFile(arrShape, SIZE);
+   
+   system("pause");
+   system("cls");
+
+   Shape* arrShape2[SIZE]{};
+   cout << "Завантаження даних із файлу у другий масив\n";
+   //Завантажити із файлу до масиву
+   loadFromFile(arrShape2, SIZE);
+   
+   print(arrShape2, SIZE);
+
+   return 0;
 }
+
+
---
+++ func.h
@@ -0,0 +1,90 @@
+#pragma once
+
+
+void print(Shape** arr, int SIZE)
+{
+    for (int i{}; i < SIZE; i++)
+    {
+        if (arr[i])
+        {
+            cout << "----------\n";
+            arr[i]->Show();
+        }
+        else 
+            cout << "В покажчику " << i << " даних немає\n";
+    }
+
+
+
+}
+
+void clearFile()
+{
+    ofstream fout;
+    fout.open("shape.txt", ios::trunc);
+    fout.close();
+}
+
+void saveToFile(Shape** arr, int SIZE)
+{
+    for (int i{}; i < SIZE; i++)
+    {
+        if (arr[i])
+        {
+            arr[i]->Save();
+        }
+        else
+            cout << "В покажчику " << i << " даних немає\n";
+    }
+    cout << "\nДанні збережено у файл\n";
+}
+
+void loadFromFile(Shape** arr, int SIZE)
+{
+    string name;
+    ifstream fin;
+
+    fin.open("shape.txt", ios::in);
+
+    if (!fin)
+    {
+        std::cerr << "Помилка читання файлу...\n";
+        exit(1);
+    }
+
+    if (fin.is_open())
+    {
+        int i{};
+        while (fin)
+        {
+            fin >> name;
+            if (name == "Квадрат")
+            {
+                arr[i] = new Square{};
+                arr[i]->Load(i);
+            }
+            else if (name == "Прямокутник")
+            {
+                arr[i] = new Reactangle{};
+                arr[i]->Load(i);
+            }
+            else if (name == "Коло")
+            {
+                arr[i] = new Circle{};
+                arr[i]->Load(i);
+            }
+            else if (name == "Еліпс")
+            {
+                arr[i] = new Elipse{};
+                arr[i]->Load(i);
+            }
+           i++;
+           getline(fin, name);
+            
+        }
+    }    
+    
+    cout << "\nДанні завантажено з файлу = кінець методу\n";
+
+    fin.close();
+}
